11fee8f0bd675f6b99b894b6f81d02b94402557e
Valery Zhitnikov <valeryzhitnikov91@gmail.com>
2022-05-29 17:35:28 +0300

    Add work

--- src/App.js
+++ src/App.js
@@ -1,25 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function New(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrap-item wrap-item-new">
+      <span className="label">New!</span>
+      {props.children}
     </div>
-  );
+  )
+};
+
+function Popular(props) {
+  return (
+    <div className="wrap-item wrap-item-popular">
+      <span className="label">Popular!</span>
+      {props.children}
+    </div>
+  )
+};
+
+function Article(props) {
+  return (
+    <div className="item item-article">
+      <h3><a href="#">{props.title}</a></h3>
+      <p className="views">Прочтений: {props.views}</p>
+    </div>
+  )
+};
+
+function Video(props) {
+  return (
+    <div className="item item-video">
+      <iframe src={props.url} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen></iframe>
+      <p className="views">Просмотров: {props.views}</p>
+    </div>
+  )
+};
+
+function withWrapper(wrapperFunc) {
+  return function(Component) {
+    const newComponent = function(props) {
+      const Wrapper = wrapperFunc(props);
+      return  ( Wrapper ? 
+        <Wrapper>
+          <Component {...props} />
+        </Wrapper> :
+        <Component {...props} />
+      )
+    }
+
+    const componentName =
+    Component.displayName || Component.name || 'Component';
+    newComponent.displayName = `WithWrapper${componentName}`;
+
+    return newComponent;
+  }
 }
 
-export default App;
+const wrapperDecorator = withWrapper(({views}) => {
+  if (views < 100) {
+    return New;
+  } else if (views > 1000) {
+    return Popular;
+  } else {
+    return null;
+  }
+});
+
+const VideoWidthWrapper = wrapperDecorator(Video);
+const ArticleWidthWrapper = wrapperDecorator(Article);
+
+function List(props) {
+  return props.list.map((item, i) => {
+    switch (item.type) {
+      case 'video':
+        return (
+          <VideoWidthWrapper key={i} {...item} />
+        );
+
+      case 'article':
+        return (
+          <ArticleWidthWrapper key={i} {...item} />
+        );
+    }
+  });
+};
+
+export default function App() {
+  const [list, setList] = useState([
+    {
+      type: 'video',
+      url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+      views: 50
+    },
+    {
+      type: 'video',
+      url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+      views: 12
+    },
+    {
+      type: 'article',
+      title: 'Невероятные события в неизвестном поселке...',
+      views: 175
+    },
+    {
+      type: 'article',
+      title: 'Секретные данные были раскрыты!',
+      views: 1532
+    },
+    {
+      type: 'video',
+      url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+      views: 4253
+    },
+    {
+      type: 'article',
+      title: 'Кот Бегемот обладает невероятной...',
+      views: 12,
+    },
+  ]);
+
+  return (
+    <List list={list} />
+  );
+}
